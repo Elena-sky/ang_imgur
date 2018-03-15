d107500306911b174c7aa90fdd3e594d0659c548
MacBook <alonasoroka1993@gmail.com>
2018-03-15 15:25:56 +0200

    search by tags

--- app/img-categories/img-categories.component.js
+++ app/img-categories/img-categories.component.js
@@ -10,15 +10,11 @@ angular
             templateUrl: 'img-categories/img-categories.template.html',
             controller:
                 function ($http) {
-                    this.$onInit = function () {
-                        // noinspection JSAnnotator
 
                         $http.get('https://api.imgur.com/3/tags')
                             .then(result => {
                                 this.cat = result.data.data.tags.slice(0,5);
                             });
-
-                    }
                 }
         }
     );
