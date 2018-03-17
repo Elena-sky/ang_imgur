08490ca6fb41ee00c9133c3a380e87f5a57ab964
MacBook <alonasoroka1993@gmail.com>
2018-03-18 01:14:30 +0200

    get images of category from API

--- app/images-of-category/images-of-category.component.js
+++ app/images-of-category/images-of-category.component.js
@@ -3,48 +3,22 @@
 // Register `phoneDetail` component, along with its associated controller and template
     +angular.
   module('imagesOfCategory',  ['ngRoute'])
-//         .component('imagesOfCategory', {
-//         // Note: The URL is relative to our `index.html` file
-//         templateUrl: 'images-of-category/images-of-category.template.html',
-//         controller:
-//             function ($http) {
-//
-//                 $http.get('https://api.imgur.com/3/tags')
-//                     .then(result => {
-//                         this.cat = result.data.data.tags;
-//
-//                     });
-//             }
-//     }
-//
-// )
         .component('imagesOfCategory', {
             templateUrl: 'images-of-category/images-of-category.template.html',
-    controller: ['$http', '$routeParams',
-        function ($http, $routeParams) {
-            var self = this;
-            self.tagSearch = $routeParams.categoryName;
+            controller: ['$http', '$routeParams',
+                function ($http, $routeParams) {
+                var self = this;
+                self.tagSearch = $routeParams.categoryName;
 
-            $http.get('https://api.imgur.com/3/gallery/hot/viral/0.json')
-                .then(result => {
-                    this.items = result.data.data;
-                    self.imgArr = [];
-                    self.imgTag = this.items.forEach(function (imgData) {
-                    self.findtag = imgData.tags.forEach(function (tagsData) {
-                        if (tagsData.name === self.tagSearch) {
-                            // self.dataImg = this.imgData;
-
-                            return self.imgArr.push(imgData);
-                        }
+                $http.get('https://api.imgur.com/3/gallery/search/?q='+self.tagSearch, {
+                    headers: {'Authorization': 'Client-ID b82eca036063cec'}
+                })
+                    .then(result => {
+                        this.imgSearch = result.data.data;
+                        console.log(this.imgSearch);
 
                     });
 
-                    });
-                    console.log(self.imgArr);
-
-                });
-
-
-        }
-    ]
+                }
+             ]
 });
